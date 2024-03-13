b712f04f597c2480c757ef145a2d66b70fd05959
ana <anastasiapak32@gmail.com>
2024-03-13 20:19:11 +0300

    sort: added assets folder

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 import Spinner from 'react-bootstrap/Spinner';
 
 const MainSpinner = () => (
-  <Spinner animation="border" role="status">
+  <Spinner animation="grow" className="ms-5 mt-3" role="status">
     <span className="visually-hidden">Loading...</span>
   </Spinner>
 );
